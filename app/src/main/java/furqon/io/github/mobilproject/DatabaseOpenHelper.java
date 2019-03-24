09d0783a04b893187d13850805fa90839b3bdb27
digitalprojects <fuzalov@gmail.com>
2019-03-24 14:46:07 +0900

    REMOVED the copy database reverted to my code

--- app/src/main/java/furqon/io/github/mobilproject/DatabaseOpenHelper.java
+++ app/src/main/java/furqon/io/github/mobilproject/DatabaseOpenHelper.java
@@ -3,87 +3,23 @@ package furqon.io.github.mobilproject;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Locale;
-
 
 public class DatabaseOpenHelper extends SQLiteOpenHelper{
-    private static final String DB_PATH = "/data/data/furqon.io.github.mobilproject/databases/";
     private static final String DB_NAME = "qurandb";
     private static final int DB_VERSION = 3;
-    public SQLiteDatabase db;
-    private Context mContext;
 
-    public DatabaseOpenHelper(Context context) {
+    DatabaseOpenHelper(Context context) {
 
         super(context, DB_NAME, null, DB_VERSION);
-        this.mContext = context;
+
     }
 
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
-    }
-    public void createDatabase() {
-        createDB();
-    }
-
-    private void createDB() {
-        boolean dbExist = DBExists();
-
-        if(!dbExist) {
-            this.getReadableDatabase();
-            copyDBFromResource();
-        }
-    }
-
-    private boolean DBExists() {
-        SQLiteDatabase db = null;
-
-        try {
-            String databasePath = DB_PATH + DB_NAME;
-            db = SQLiteDatabase.openDatabase(databasePath, null, SQLiteDatabase.OPEN_READWRITE);
-            db.setLocale(Locale.getDefault());
-            db.setVersion(DB_VERSION);
-        } catch (SQLiteException e) {
-            Log.e("SqlHelper", "database not found");
-        }
-
-        if (db != null) {
-            db.close();
-        }
-        return db != null ? true : false;
-    }
-    private void copyDBFromResource() {
-        InputStream inputStream = null;
-        OutputStream outStream = null;
-        String dbFilePath = DB_PATH + DB_NAME;
-
-        try {
-            inputStream = mContext.getAssets().open(DB_NAME);
-            outStream = new FileOutputStream(dbFilePath);
-
-            byte[] buffer = new byte[1024];
-            int length;
-            while ((length = inputStream.read(buffer)) > 0) {
-                outStream.write(buffer, 0, length);
-            }
-
-            outStream.flush();
-            outStream.close();
-            inputStream.close();
-
-        } catch (IOException e) {
-            throw new Error("Problem copying database from resource file.");
-        }
 
     }
 
