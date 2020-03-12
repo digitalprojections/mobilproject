00e33b7e2d5c981e70576573dca99eb6da562475
digitalprojects <fuzalov@gmail.com>
2020-03-13 08:47:11 +0900

    adding audio on notofication bar trying to add as foreground service

--- app/src/main/java/furqon/io/github/mobilproject/Services/AudioPlayerService.java
+++ app/src/main/java/furqon/io/github/mobilproject/Services/AudioPlayerService.java
@@ -2,6 +2,7 @@ package furqon.io.github.mobilproject.Services;
 
 
 import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
@@ -9,9 +10,11 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.MediaPlayer;
+import android.media.session.MediaController;
 import android.media.session.MediaSession;
 import android.os.Build;
 import android.os.IBinder;
+import android.os.RemoteException;
 import android.support.v4.media.session.MediaControllerCompat;
 import android.support.v4.media.session.MediaSessionCompat;
 
@@ -33,9 +36,9 @@ public class AudioPlayerService extends Service {
     public static final String ACTION_STOP = "action_stop";
 
     private MediaPlayer mediaPlayer;
-    private MediaSessionCompat mediaSession;
+    private MediaSession mediaSession;
     private MediaSessionManager mediaSessionManager;
-    private MediaControllerCompat controllerCompat;
+    private MediaController controllerCompat;
     static PendingIntent pendingIntentPrev;
     static PendingIntent pendingIntentPlay;
     static PendingIntent pendingIntentNext;
@@ -79,71 +82,86 @@ public class AudioPlayerService extends Service {
         }
     }
 
-//
-//    private Notification.Action generateAction(int icon, String title, String intentAction){
-//        Intent intent = new Intent(getApplicationContext(), AudioPlayerService.class);
-//        intent.setAction(intentAction);
-//        PendingIntent pendingIntent = PendingIntent.getService(getApplicationContext(), 1, intent, 0);
-//        Notification.Action build;
-//        build = new Notification.Action(icon, title, pendingIntent);
-//        return build; //make sure to build where necessary
-//    }
-
-    public static void ShowNotification(Context context, int playbutton, String suranomi, int pos) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
-            MediaSessionCompat mediaSessionCompat = new MediaSessionCompat(context, "tag");
-
-            int drw_previous;
-            if (pos == 0) {
-                pendingIntentPrev = null;
-                drw_previous = 0;
-            } else {
-                Intent intentPrevious = new Intent(context, NotificationActionService.class)
-                        .setAction(Furqon.ACTION_PREV);
-
-                pendingIntentPrev = PendingIntent.getBroadcast(context, 0, intentPrevious, PendingIntent.FLAG_UPDATE_CURRENT);
-            }
-
-            Intent intentPlay = new Intent(context, NotificationActionService.class)
-                    .setAction(Furqon.ACTION_PLAY);
-            pendingIntentPlay = PendingIntent.getBroadcast(context, 0, intentPlay, PendingIntent.FLAG_UPDATE_CURRENT);
-
-
-            int drw_next;
-            if (pos == 0) {
-                pendingIntentNext = null;
-                drw_next = 0;
-            } else {
-                Intent intentNext = new Intent(context, NotificationActionService.class)
-                        .setAction(Furqon.ACTION_NEXT);
-
-                pendingIntentNext = PendingIntent.getBroadcast(context, 0, intentNext, PendingIntent.FLAG_UPDATE_CURRENT);
-            }
-
-
-            Bitmap audio_player_icon = BitmapFactory.decodeResource(context.getResources(), R.drawable.nightsky);
-
-            Notification notification;
-            notification = new NotificationCompat.Builder(context, Furqon.AUDIO_PLAYING_NOTIFICATION_CHANNEL)
-                    .setSmallIcon(R.drawable.ic_surah_audio_24dp)
-                    .setLargeIcon(audio_player_icon)
-                    .setContentTitle("Furqon Audio Player")
-                    .setContentText(suranomi)
-                    .addAction(R.drawable.ic_previous, context.getString(R.string.fast_rewind), pendingIntentPrev)
-                    .addAction(playbutton, context.getString(R.string.play), pendingIntentPlay)
-                    .addAction(R.drawable.ic_next, context.getString(R.string.fast_forward), pendingIntentNext)
-                    .addAction(R.drawable.ic_favorite_border_black_24dp, context.getString(R.string.favorites), pendingIntentFav)
-                    .setStyle(new androidx.media.app.NotificationCompat.MediaStyle()
-                            .setShowActionsInCompactView(0, 1, 2)
-                            .setMediaSession(mediaSessionCompat.getSessionToken()))
-                    .setPriority(NotificationCompat.PRIORITY_LOW)
-                    .setAutoCancel(false)
-                    .setOnlyAlertOnce(true)
-                    .build();
-            notificationManagerCompat.notify(1, notification);
-        }
+    private Notification.Action generateAction(int icon, String title, String intentAction){
+        Intent intent = new Intent(getApplicationContext(), AudioPlayerService.class);
+        intent.setAction(intentAction);
+        PendingIntent pendingIntent = PendingIntent.getService(getApplicationContext(), 1, intent, 0);
+        Notification.Action build;
+        build = new Notification.Action(icon, title, pendingIntent);
+        return build; //make sure to build where necessary
     }
+
+    private void buildNotification(Notification.Action action){
+        Notification.MediaStyle style = new Notification.MediaStyle();
+        Intent intent = new Intent(getApplicationContext(), AudioPlayerService.class);
+        intent.setAction(ACTION_STOP);
+        PendingIntent pendingIntent = PendingIntent.getService(getApplicationContext(), 1, intent, 0);
+        Notification.Builder builder = new Notification.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher2)
+                .setContentTitle(getString(R.string.audio_player_title))
+                .setContentText("Surah name")
+                .setDeleteIntent(pendingIntent)
+                .setStyle(style);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, builder.build());
+    }
+
+//    public static void ShowNotification(Context context, int playbutton, String suranomi, int pos) {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
+//            MediaSessionCompat mediaSessionCompat = new MediaSessionCompat(context, "tag");
+//
+//            int drw_previous;
+//            if (pos == 0) {
+//                pendingIntentPrev = null;
+//                drw_previous = 0;
+//            } else {
+//                Intent intentPrevious = new Intent(context, NotificationActionService.class)
+//                        .setAction(Furqon.ACTION_PREV);
+//
+//                pendingIntentPrev = PendingIntent.getBroadcast(context, 0, intentPrevious, PendingIntent.FLAG_UPDATE_CURRENT);
+//            }
+//
+//            Intent intentPlay = new Intent(context, NotificationActionService.class)
+//                    .setAction(Furqon.ACTION_PLAY);
+//            pendingIntentPlay = PendingIntent.getBroadcast(context, 0, intentPlay, PendingIntent.FLAG_UPDATE_CURRENT);
+//
+//
+//            int drw_next;
+//            if (pos == 0) {
+//                pendingIntentNext = null;
+//                drw_next = 0;
+//            } else {
+//                Intent intentNext = new Intent(context, NotificationActionService.class)
+//                        .setAction(Furqon.ACTION_NEXT);
+//
+//                pendingIntentNext = PendingIntent.getBroadcast(context, 0, intentNext, PendingIntent.FLAG_UPDATE_CURRENT);
+//            }
+//
+//
+//            Bitmap audio_player_icon = BitmapFactory.decodeResource(context.getResources(), R.drawable.nightsky);
+//
+//            Notification notification;
+//            notification = new NotificationCompat.Builder(context, Furqon.AUDIO_PLAYING_NOTIFICATION_CHANNEL)
+//                    .setSmallIcon(R.drawable.ic_surah_audio_24dp)
+//                    .setLargeIcon(audio_player_icon)
+//                    .setContentTitle("Furqon Audio Player")
+//                    .setContentText(suranomi)
+//                    .addAction(R.drawable.ic_previous, context.getString(R.string.fast_rewind), pendingIntentPrev)
+//                    .addAction(playbutton, context.getString(R.string.play), pendingIntentPlay)
+//                    .addAction(R.drawable.ic_next, context.getString(R.string.fast_forward), pendingIntentNext)
+//                    .addAction(R.drawable.ic_favorite_border_black_24dp, context.getString(R.string.favorites), pendingIntentFav)
+//                    .setStyle(new androidx.media.app.NotificationCompat.MediaStyle()
+//                            .setShowActionsInCompactView(0, 1, 2)
+//                            .setMediaSession(mediaSessionCompat.getSessionToken()))
+//                    .setPriority(NotificationCompat.PRIORITY_LOW)
+//                    .setAutoCancel(false)
+//                    .setOnlyAlertOnce(true)
+//                    .build();
+//            notificationManagerCompat.notify(1, notification);
+//        }
+//    }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
@@ -153,9 +171,23 @@ public class AudioPlayerService extends Service {
         return super.onStartCommand(intent, flags, startId);
     }
 
-    private void initMediaSession() {
+    private void initMediaSession(){
         mediaPlayer = new MediaPlayer();
-        mediaSession = new MediaSessionCompat(getApplicationContext(), getString(R.string.audio_player_title));
+        mediaSession = new MediaSession(getApplicationContext(), getString(R.string.audio_player_title));
+        controllerCompat = new MediaController(getApplicationContext(), mediaSession.getSessionToken());
+        mediaSession.setCallback(new MediaSession.Callback() {
+            @Override
+            public void onPlay() {
+                super.onPlay();
+                buildNotification(generateAction(R.drawable.ic_pause_circle, "Pause", ACTION_PLAY));
+            }
+
+            @Override
+            public void onPause() {
+                super.onPause();
+                buildNotification(generateAction(R.drawable.ic_play_circle, "Play", ACTION_PAUSE));
+            }
+        });
     }
 
     @Override
