69435dc75e6342579f7470e0d92cce609f919383
digitalprojects <fuzalov@gmail.com>
2020-03-16 23:45:46 +0900

    creating earn coin activity

---
+++ app/src/main/java/furqon/io/github/mobilproject/EarnCoinsActivity.java
@@ -0,0 +1,14 @@
+package furqon.io.github.mobilproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EarnCoinsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_earn_coins);
+    }
+}
