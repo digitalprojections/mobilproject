5a347042080260ce7b2765a86b0049a629c4a61b
digitalprojects <fuzalov@gmail.com>
2020-03-16 23:02:59 +0900

    change strategy of download

---
+++ app/src/main/java/furqon/io/github/mobilproject/ExtraActivity.java
@@ -0,0 +1,14 @@
+package furqon.io.github.mobilproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExtraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_extra);
+    }
+}
