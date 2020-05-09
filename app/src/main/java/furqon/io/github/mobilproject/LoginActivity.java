2d416d03552b05b95c6b8de004939b60d3352b85
digitalprojections <fuzalov@gmail.com>
2020-05-09 23:40:51 +0900

    adding google sign in

--- app/src/main/java/furqon/io/github/mobilproject/LoginActivity.java
+++ app/src/main/java/furqon/io/github/mobilproject/LoginActivity.java
@@ -19,14 +19,22 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.crashlytics.android.Crashlytics;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInApi;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.dynamiclinks.FirebaseDynamicLinks;
 import com.google.firebase.dynamiclinks.PendingDynamicLinkData;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
@@ -61,6 +69,8 @@ public class LoginActivity extends AppCompatActivity {
     private RequestQueue queue;
     private FirebaseRemoteConfig mFirebaseRemoteConfig;
     private SharedPreferences mSharedPref;
+    private GoogleSignInClient mGoogleSignInClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,6 +91,22 @@ public class LoginActivity extends AppCompatActivity {
         //--------------------------------------------------------
         mAuth = FirebaseAuth.getInstance();
         currentUser = mAuth.getCurrentUser();
+// [START config_signin]
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        // [END config_signin]
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, 1);
+            }
+        });
         //========================================================
         mSharedPref = SharedPreferences.getInstance();
         mSharedPref.init(getApplicationContext());
@@ -97,7 +123,7 @@ public class LoginActivity extends AppCompatActivity {
         }
 
 
-        updateUI();
+        updateUI(currentUser);
         if (mSharedPref.isFirstRun()) {
             Intent intent = new Intent(this, ScrollingActivity.class);
             startActivity(intent);
@@ -125,7 +151,47 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == 1) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                // ...
+            }
+        }
+    }
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            //Snackbar.make(mBinding.mainLayout, "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
     public void CheckRC() {
         Log.d(TAG, mFirebaseRemoteConfig.getString("rewardad_multiplier") + " reward multiplier");
         mFirebaseRemoteConfig.fetchAndActivate()
@@ -164,7 +230,26 @@ public class LoginActivity extends AppCompatActivity {
                             Log.d(TAG, "signInAnonymously:success");
 
                             currentUser = mAuth.getCurrentUser();
-                            updateUI();
+                            AuthCredential credential = GoogleAuthProvider.getCredential( getString(R.string.default_web_client_id), null);
+                            mAuth.getCurrentUser().linkWithCredential(credential)
+                                    .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<AuthResult> task) {
+                                            if (task.isSuccessful()) {
+                                                Log.d(TAG, "linkWithCredential:success");
+                                                FirebaseUser user = task.getResult().getUser();
+                                                updateUI(user);
+                                            } else {
+                                                Log.w(TAG, "linkWithCredential:failure", task.getException());
+                                                Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                                        Toast.LENGTH_SHORT).show();
+                                                updateUI(null);
+                                            }
+
+                                            // ...
+                                        }
+                                    });
+                            //updateUI(currentUser);
 
 
                         } else {
@@ -183,13 +268,14 @@ public class LoginActivity extends AppCompatActivity {
         Log.e(TAG, "Signin ATTEMPT: " + mAuth);
     }
 
-    private void updateUI() {
+    private void updateUI(FirebaseUser currentUser) {
         boolean isSignedIn = (currentUser != null);
 
         // Status text
         if (isSignedIn) {
             Log.i(TAG, "SIGNED IN. FIREBASE AUTH " + currentUser.getUid());
             sendRegistrationToServer();
+            //findViewById(R.id.sign_in_button).setVisibility(View.GONE);
             //checkAppSignature(this);
         } else {
             signInAnonymously();
